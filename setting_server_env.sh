64d1f535ac01adae11b23aefae4f785a7b96797b
Toshihiro Takehara aka nouphet <nouphet.ratanavong@gmail.com>
2012-08-30 11:16:33 +0900

    add epel for CentOS 5

--- setting_server_env.sh
+++ setting_server_env.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-yum -y install screen git tree dstat
+yum -y install yum-priorities
+
+## add epel repository for CentOS 5
+# cd /usr/local/src/
+# wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/5/i386/epel-release-5-4.noarch.rpm
+# rpm -ivh epel-release-5-4.noarch.rpm
+
+yum -y install screen git tree dstat etckeep
 gem install rak
 
 chkconfig ntpd on
