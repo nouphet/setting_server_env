5d8bdd08da90d5a38d8821ba0773450fdd17010c
Toshihiro Takehara aka nouphet <nouphet.ratanavong@gmail.com>
2012-09-15 23:12:31 +0900

    fix epel 6.5 to 6.7

--- setting_server_env.sh
+++ setting_server_env.sh
@@ -15,12 +15,12 @@ echo "
 
 ## add epel repository for CentOS 6 64bit
 # cd /usr/local/src/
-# wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-5.noarch.rpm
+# wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-7.noarch.rpm
 # rpm -ivh epel-release-6-5.noarch.rpm
 
 ## add epel repository for CentOS 6 32bit
 # cd /usr/local/src/
-# wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/i386/epel-release-6-5.noarch.rpm
+# wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/i386/epel-release-6-7.noarch.rpm
 # rpm -ivh epel-release-6-5.noarch.rpm
 "
 
