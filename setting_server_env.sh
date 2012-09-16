bba72829b44213e0f5923e3e8fcd0601ad992b62
Toshihiro Takehara aka nouphet <nouphet.ratanavong@gmail.com>
2012-09-16 22:49:23 +0900

    add judgment of OS

--- setting_server_env.sh
+++ setting_server_env.sh
@@ -1,28 +1,64 @@
 #!/bin/bash
 
-# get config files
-cd ~
-wget https://raw.github.com/nouphet/dotfiles/master/dot.gitconfig
-mv dot.gitconfig .gitconfig
+USER=`whoami`
 
+if [ "$USER" != 'root' ]
+then
+    echo "rootでコマンドを実行してください。"
+    exit 1
+fi
+
+# get config files
+cd ~/
+wget https://raw.github.com/nouphet/dotfiles/master/dot.gitconfig
+mv dot.gitconfig ~/.gitconfig
+
+## install yum-priorities
 yum -y install yum-priorities
 
-echo "
-## add epel repository for CentOS 5 32bit
-# cd /usr/local/src/
-# wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/5/i386/epel-release-5-4.noarch.rpm
-# rpm -ivh epel-release-5-4.noarch.rpm
-
 ## add epel repository for CentOS 6 64bit
-# cd /usr/local/src/
-# wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-7.noarch.rpm
-# rpm -ivh epel-release-6-7.noarch.rpm
-
-## add epel repository for CentOS 6 32bit
-# cd /usr/local/src/
-# wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/i386/epel-release-6-7.noarch.rpm
-# rpm -ivh epel-release-6-7.noarch.rpm
-"
+if [ -f /etc/redhat-release ]
+then
+    CHK=`egrep "CentOS release 5|Red Hat Enterprise Linux .* 5" /etc/redhat-release`
+    if [ "$CHK" != '' ]
+    then
+        if [ `uname -a | grep x86_64 | awk '{ print $12 }'` == "x86_64" ]
+        then
+            echo ""
+            echo "RHEL 5.x / CentOS 5.x / OEL 5.x x86_64 が検出されました。"
+            # add epel repository for CentOS 5 32bit
+            cd /usr/local/src/
+            wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/5/i386/epel-release-5-4.noarch.rpm
+            rpm -ivh epel-release-5-4.noarch.rpm
+        else
+            echo ""
+            echo "RHEL 5.x / CentOS 5.x / OEL 5.x 386 が検出されました。"
+            echo "対象のEPELリポジトリが設定されていません。"
+            #exit 1
+        fi
+        # Stop Services for CentOS 5
+        chkconfig yum-updatesd off
+        chkconfig pcscd off
+        chkconfig bluetooth off
+    else
+        if [ `uname -a | grep x86_64 | awk '{ print $12 }'` == "x86_64" ]
+        then
+            echo ""
+            echo "RHEL 6.x / CentOS 6.x / OEL 6.x x86_64 が検出されました。"
+            cd /usr/local/src/
+            wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-7.noarch.rpm
+            rpm -ivh epel-release-6-7.noarch.rpm
+        else
+            echo ""
+            echo "RHEL 6.x / CentOS 6.x / OEL 6.x 386 が検出されました。"
+            echo "対象のEPELリポジトリが設定されていません。"
+            # add epel repository for CentOS 6 32bit
+            cd /usr/local/src/
+            wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/i386/epel-release-6-7.noarch.rpm
+            rpm -ivh epel-release-6-7.noarch.rpm
+        fi
+    fi
+fi
 
 yum -y install screen git tree dstat etckeeper
 cd /etc
@@ -42,10 +78,6 @@ chkconfig ntpd on
 chkconfig ntpd --list
 /etc/init.d/ntpd start
 
-# Stop Services for CentOS 5
-chkconfig yum-updatesd off
-chkconfig pcscd off
-chkconfig bluetooth off
 
 # disable SELinux
 getenforce
@@ -56,3 +88,7 @@ vim /etc/sysconfig/selinux
 # disable ip6tables
 chkconfig ip6tables off
 chkconfig ip6tables --list
+
+echo ""
+echo "サーバをリブートして下さい。"
+echo "コマンドを実行してください。 reboot"
