afef69841cffb732133e760e818687a656dccc92
Toshihiro Takehara aka nouphet <nouphet.ratanavong@gmail.com>
2013-01-04 23:41:58 +0900

    add packages.

--- setting_xoops_env.sh
+++ setting_xoops_env.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-yum -y install php php-mysql php-mbstring php-pdo mysql-server httpd
+yum -y install php php-mysql php-mbstring php-mcrypt php-pdo mysql-server httpd php-xml php-xmlrpc php-gd ImageMagick curl libcurl
 
 chkconfig httpd on
 chkconfig mysqld on
+
+/etc/init.d/httpd start
+/etc/init.d/mysqld start
+
+echo "Finish Environment Build, and Middle Ware Started"
 
